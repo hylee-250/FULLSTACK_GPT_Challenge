62546571f412ba99247b56315d8cdffa1e0c6cf5
이호영 <hylee@MacBook-Pro.local>
2024-12-20 04:03:18 +0900

    error resolved

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from langchain.memory import ConversationBufferMemory
 import streamlit as st
 
 @ st.cache_resource(show_spinner="Embedding file...")
-def embed_file(file):
+def embed_file(file, my_api_key):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
     with open(file_path, "wb") as f:
@@ -72,7 +72,7 @@ with st.sidebar:
 
 llm = ChatOpenAI(
     temperature=0.1,
-    api_key=my_api_key,
+    openai_api_key=my_api_key,
 )
 
 memory = ConversationBufferMemory(
@@ -97,7 +97,7 @@ prompt = ChatPromptTemplate.from_messages(
 
 
 if file:
-    retriever = embed_file(file)
+    retriever = embed_file(file, my_api_key)
     send_message("I'm ready! Ask away!", "ai", save=False)
     paint_history()
     message = st.chat_input("Ask anything about your file...")
