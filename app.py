5c7eb838f5e26c7d58d771e1b666ed03df99a792
이호영 <hylee@MacBook-Pro.local>
2024-12-20 03:19:03 +0900

    streamlit app

---
+++ app.py
@@ -0,0 +1,125 @@
+from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.storage import LocalFileStore
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.memory import ConversationBufferMemory
+import streamlit as st
+
+@ st.cache_resource(show_spinner="Embedding file...")
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = TextLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings(openai_api_key=my_api_key)
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state["messages"].append({"message":message,"role":role})
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(
+            message["message"],
+            message["role"],
+            save=False
+        )
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+st.title("DocumentGPT Challenge")
+
+st.markdown(
+    """
+Welcome!
+            
+Use this chatbot to ask questions to an AI about your files!
+
+Upload your files on the sidebar.
+
+First Enter your OpenAI API Key!
+
+"""
+)
+with st.sidebar:
+    my_api_key= st.text_input("Enter your OpenAI API Key",type="password")
+    
+    file = st.file_uploader(
+        "Upload a .txt file",
+        type=["txt"],
+        )
+try:
+    llm = ChatOpenAI(
+        temperature=0.1,
+        api_key=my_api_key,
+    )
+
+    memory = ConversationBufferMemory(
+    llm=llm,
+    max_token_limit=20,
+    return_messages=True,
+    )
+except:
+    pass
+
+
+def load_memory(_):
+    return memory.load_memory_variables({})["history"]
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant. Answer questions using only the following context. If you don't know the answer just say you don't know, don't make it up:\n\n{context}",
+        ),
+        MessagesPlaceholder(variable_name="history"),
+        ("human", "{question}"),
+    ]
+)
+
+try:
+    if file:
+        retriever = embed_file(file)
+        send_message("I'm ready! Ask away!", "ai", save=False)
+        paint_history()
+        message = st.chat_input("Ask anything about your file...")
+        if message:
+            send_message(message, "human")
+            chain = (
+            {
+                "context": retriever | RunnableLambda(format_docs),
+                "question": RunnablePassthrough(),
+                "history": load_memory,
+            }
+            | prompt
+            | llm
+            )
+            response = chain.invoke(message)
+            send_message(response.content, "ai")
+
+    else:
+        st.session_state["messages"] = []
+except:
+    st.error("Enter your OpenAI Key First!")
+    pass
